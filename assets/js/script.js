3a56c45f0c01aa8ed7b02e6de552a6f9aa9ecec5
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-03-19 11:23:04 -0400

    Initalize js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 // declared variables
+var start_btn = document.querySelector(".start_btn button");
+var info_box = document.querySelector(".info_box");
+var exit_btn = document.querySelector(".buttons .quit");
+var continue_btn = document.querySelector(".buttons .restart");
+
+
+// when start button is clicked
+start_btn.onclick = function () {
+    info_box.classList.add("activeInfo");
+}
 
 
 
